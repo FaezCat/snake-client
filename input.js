cfaa2d4fec68ceb817a7d408fe02e290f2261d8d
faez <faezc@hotmail.com>
2021-09-22 23:03:58 +0000

    refactored play file to only have the connect and setupInput function calls, moved setupInput function into separate, input, file

---
+++ input.js
@@ -0,0 +1,18 @@
+// this function closes the program if CTRL + C are passed into the terminal line
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+// this function sets up our ability to listen to what is inputted into the command line, then begins listening for user input and handles if CTRL + C is inputted
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = { setupInput };
